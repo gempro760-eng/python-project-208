2cf0fb77d9a4f1b43e34addc7bc55b6d7c622c1e
Natchlazen <gempro760@gmail.com>
2026-08-22 10:42:55 -0500

    feat: integrar bootstrap 5 y renderizado de plantilla principal

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1>¡Bienvenido al Gestor de Tareas!</h1>")
+    return render(request, 'index.html')
